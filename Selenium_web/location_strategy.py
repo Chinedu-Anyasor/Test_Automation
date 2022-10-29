c915f8d63b01ed7f8089613df51a23c5519a0f93
Nedoxy <nk.nedum@gmail.com>
2022-10-29 23:17:18 +0100

    Location Strategy in selenium

--- Selenium_web/location_strategy.py
+++ Selenium_web/location_strategy.py
@@ -8,7 +8,14 @@ def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://www.testifyltd.com")
     hero_element = driver.find_element(By.TAG_NAME, "h1")
-    print("Hero Element", hero_element)
+    print("Hero Element", hero_element, hero_element.text)
+
+    hero_element = driver.find_element(By.CLASS_NAME, "react-reveal")
+    print("Hero Element", hero_element.text)
+
+    links = driver.find_elements(By.TAG_NAME, "a")
+    for link in links:
+        print(link.text)
 
 
 if __name__ == '__main__':
