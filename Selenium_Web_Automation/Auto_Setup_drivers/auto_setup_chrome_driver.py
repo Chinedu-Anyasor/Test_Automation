5f1e024e171af38567b774c42042657ea5b270a9
Nedoxy <nk.nedum@gmail.com>
2022-11-15 00:48:39 +0100

    auto_setup_chrome_driver

--- Selenium_Web_Automation/Auto_Setup_drivers/auto_setup_chrome_driver.py
+++ Selenium_Web_Automation/Auto_Setup_drivers/auto_setup_chrome_driver.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+
 def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://google.com")
