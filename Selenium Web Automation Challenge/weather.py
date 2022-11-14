39e53ff036daa551f02c714575c5279f21001268
Nedoxy <nk.nedum@gmail.com>
2022-11-14 14:46:22 +0100

    Weather Report

--- Selenium Web Automation Challenge/weather.py
+++ Selenium Web Automation Challenge/weather.py
@@ -6,10 +6,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def send_keys_to_element(element, *keys):
-    element.send_keys(keys)
-
-
 def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://weather.com/")
