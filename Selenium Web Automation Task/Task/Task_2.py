e53b0128128162afb1ffe14d6bece22148d19c4b
Nedoxy <nk.nedum@gmail.com>
2022-11-14 13:46:33 +0100

    Task 2

--- Selenium Web Automation Task/Task/Task_2.py
+++ Selenium Web Automation Task/Task/Task_2.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+
+
 def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://academy.testifyltd.com")
