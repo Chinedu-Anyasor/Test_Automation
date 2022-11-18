f690e4c427ad832c4de41779d84aa80c85bdfbd2
Nedoxy <nk.nedum@gmail.com>
2022-11-18 13:53:15 +0100

    Personal Project

--- PersonalProject/firstTestCase.py
+++ PersonalProject/firstTestCase.py
@@ -9,29 +9,46 @@
 # 7. Verify title of the page OrangeHRM (Expected title)
 # 8. Close Browser
 
-
+# Import all the webdriver modules
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-# 1:
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# 2:
-driver.get("https://opensourse-demo.orangehrmlive.com/")
-# 3:
-driver.find_element(By.NAME, "txtUsername").send_keys("Admin")
-# 4:
-driver.find_element(By.ID, "divPassword").send_keys("admin123")
-# 5:
+# Specify the browser location
+serv_obj = Service(ChromeDriverManager().install())
+# Specify the Url
+
+url = "https://opensourse-demo.orangehrmlive.com/auth/login/"
+
+# 1: Create the Webdriver Object
+driver = webdriver.Chrome(service=serv_obj)
+
+# 2: Open Url
+driver.get(url)
+
+# 3: Enter Username
+# clear the value in the field and enter username
+driver.find_element(By.NAME, "txtUsername").clear()
+driver.find_element(By.NAME, "txtUsername").send_keys("admin")
+
+# 4: Enter Password
+# clear the value in the field and enter password
+driver.find_element(By.ID, "txtPassword").clear()
+driver.find_element(By.ID, "txtPassword").send_keys("admin123")
+
+# 5: Click on login
 driver.find_element(By.ID, "btnLogin").click()
-# 6:
+
+# 6: Capture the title of the homepage
 actual_title = driver.title
 expected_title = "OrangeHRM"
-# 7:
+
+# 7: Verify the title of the page
 if actual_title == expected_title:
     print("Login Test Passed")
 else:
     print("Login Test Failed")
-# 8:
+
+# 8: Close browser
 driver.close()
